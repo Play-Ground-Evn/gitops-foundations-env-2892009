cf20ddcdbdd1eddf36eed2695f1de5b77edfd0ba
Mbuaku Franklin F <mbuaku@gmail.com>
2024-12-06 15:52:28 -0800

    ran the setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+#find . -type f -exec gsed -i 's/cloudmastershub/'$1'/g' {} + # This works on MacBook
+find . -type f -exec sed -i 's/cloudmastershub/'$1'/g' {} + #This is the original command for Linux
